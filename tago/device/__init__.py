fc948622173f0c7e650f1a2d7cba994bdc5abf79
Felipe Lima <felipe@ferreiradelima.com>
2016-03-15 13:46:34 +1300

    changed realtime endpoint

--- tago/device/__init__.py
+++ tago/device/__init__.py
@@ -4,8 +4,8 @@ import os
 from socket import TagoRealTime
 
 API_TAGO = os.environ.get('TAGO_SERVER') or 'https://api.tago.io'
-REALTIME = os.environ.get('TAGO_REALTIME') or 'realtime.tago.io'
-REALTIME_PORT = int(os.environ.get('TAGO_REALTIME_PORT') or 80)
+REALTIME = os.environ.get('TAGO_REALTIME') or 'api.tago.io'
+REALTIME_PORT = int(os.environ.get('TAGO_REALTIME_PORT') or 85)
 
 class Device:
     def __init__(self, token):
