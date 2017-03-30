0e6d14071690da9a5c0d83f38a500ece2fe958cd
Kushagra Mishra <kmishra@ncsu.edu>
2017-03-30 10:09:59 -0400

    moving analysis test to examples

--- tago/analysis/test.py
+++
@@ -1,8 +0,0 @@
-import Analysis
-
-def parsePayload(context, scope):
-    print 'Hello World'
-    print context
-    print scope
-
-my_analysis = Analysis(parsePayload, '606109f0-bda3-11e6-a1e0-f13b10826644')
