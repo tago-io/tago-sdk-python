1280d6f9a99c83867c385b51af283b7074632c0d
vchawla3 <vchawla3@ncsu.edu>
2017-04-22 12:47:53 -0400

    finished actions class

---
+++ tago/account/actions.py
@@ -0,0 +1,31 @@
+import requests # Used to make HTTP requests
+import json # Used to parse JSON
+import os # Used to infer environment variables
+
+class Actions:
+	def __init__(self, token):
+        self.token = token
+        self.default_headers = { 'content-type': 'application/json', 'Device-Token': token }
+
+        return
+
+    def list(self):
+        return requests.get('{api_endpoint}/action'.format(api_endpoint=API_TAGO), headers=self.default_headers).json()
+
+    def create(self, data):
+    	data = data if data else {}
+    	return requests.post('{api_endpoint}/action'.format(api_endpoint=API_TAGO), headers=self.default_headers, data=json.dumps(data)).json()
+
+    def edit(self, action_id, data):
+    	data = data if data else {}
+    	return requests.put('{api_endpoint}/action/{action_id}'.format(api_endpoint=API_TAGO, action_id=action_id), headers=self.default_headers, data=json.dumps(data)).json()
+
+    def delete(self, action_id):
+    	return requests.delete('{api_endpoint}/action/{action_id}'.format(api_endpoint=API_TAGO, action_id=action_id), headers=self.default_headers).json()
+
+    def info(self, action_id):
+    	# if action_id is null, then call list
+    	if action_id is None or action_id == '':
+	    	return self.list()
+
+    	return requests.get('{api_endpoint}/action/{action_id}'.format(api_endpoint=API_TAGO, action_id=action_id), headers=self.default_headers).json()
