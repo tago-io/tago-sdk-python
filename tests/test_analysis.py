01e0ce7d36d933782daa4b97c5ea323cd5b9f64c
Zachery DeLong <zacherypd@gmail.com>
2017-04-22 19:53:36 -0400

    Starting on analysis tests.
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -3,19 +3,148 @@ from tago.account.analysis import Analysis as Analysis
 import os
 
 TOKEN = os.environ.get('TAGO_TOKEN_ACCOUNT') or 'TOKEN'
-DEBUG_MESSAGE = "The response to {} \n{}"
+DEBUG_MESSAGE = "The response to {} \n{}\n"
+
+testAnalysis = Analysis(TOKEN)
 
 def test_analysis_create_delete():
-    assert False
+    ###########################
+    # Creating a new analysis
+    ###########################
+    analysisResult = testAnalysis.create({ "name": "Test Analysis" })
+
+    print DEBUG_MESSAGE.format('analysis creation', analysisResult)
+
+    if analysisResult['status']:
+        assert True
+    else:
+        assert False
+    
+    ###########################
+    # Deleting the new analysis
+    ###########################
+    analysisResult = testAnalysis.delete(analysisResult['result']['id'])
+
+    print DEBUG_MESSAGE.format('analysis deletion', analysisResult)
+
+    if analysisResult['status']:
+        assert True
+    else:
+        assert False
+
 
 def test_analysis_edit():
-    assert False
+    ###########################
+    # Creating a new analysis
+    ###########################
+    analysisResult = testAnalysis.create({ "name": "Test Analysis" })
+
+    print DEBUG_MESSAGE.format('analysis creation', analysisResult)
+
+    if analysisResult['status']:
+        assert True
+    else:
+        assert False
+
+    ###########################
+    # Editing the new analysis
+    ###########################
+    editResult = testAnalysis.edit(analysisResult['result']['id'], {"name": "A new analysis name"})
+
+    if editResult['status']:
+        assert True
+    else:
+        assert False
+    
+    ###########################
+    # Deleting the new analysis
+    ###########################
+    analysisResult = testAnalysis.delete(analysisResult['result']['id'])
+
+    print DEBUG_MESSAGE.format('analysis deletion', analysisResult)
+
+    if analysisResult['status']:
+        assert True
+    else:
+        assert False
 
 def test_analysis_info():
-    assert False
+    ###########################
+    # Creating a new analysis
+    ###########################
+    analysisResult = testAnalysis.create({ "name": "Test Analysis" })
+
+    print DEBUG_MESSAGE.format('analysis creation', analysisResult)
+
+    if analysisResult['status']:
+        assert True
+    else:
+        assert False
+
+    ###########################
+    #  Retrieving analysis info
+    ###########################
+    infoResult = testAnalysis.info(analysisResult['result']['id'])
+
+    print DEBUG_MESSAGE.format('analysis information', infoResult)
+
+    if infoResult['status']:
+        assert True
+    else:
+        assert False
+
+    ###########################
+    # Deleting the new analysis
+    ###########################
+    analysisResult = testAnalysis.delete(analysisResult['result']['id'])
+
+    print DEBUG_MESSAGE.format('analysis deletion', analysisResult)
+
+    if analysisResult['status']:
+        assert True
+    else:
+        assert False
 
 def test_analysis_tokenGenerate():
-    assert False
+    ###########################
+    # Creating a new analysis
+    ###########################
+    analysisResult = testAnalysis.create({ "name": "Test Analysis" })
+
+    print DEBUG_MESSAGE.format('analysis creation', analysisResult)
+
+    if analysisResult['status']:
+        assert True
+    else:
+        assert False
+
+    ###########################
+    # Generating new token
+    ###########################
+    tokenResult = testAnalysis.tokenGenerate(analysisResult['result']['id'])
+
+    print DEBUG_MESSAGE.format('analysis token generation', tokenResult)
+
+    if tokenResult:
+        assert True
+    else:
+        assert False
+
+    ###########################
+    # Deleting up new token (is this even possible?)
+    ###########################
+    
+    ###########################
+    # Deleting the new analysis
+    ###########################
+    analysisResult = testAnalysis.delete(analysisResult['result']['id'])
+
+    print DEBUG_MESSAGE.format('analysis deletion', analysisResult)
+
+    if analysisResult['status']:
+        assert True
+    else:
+        assert False
 
 def test_analysis_uploadFile():
     assert False
