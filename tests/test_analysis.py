5d2f4f15b59d5508dc9c61a02a1d74beac01c862
Zachery DeLong <zacherypd@gmail.com>
2017-04-22 17:33:08 -0400

    Scaffolding analysis methods.
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

---
+++ tests/test_analysis.py
@@ -0,0 +1,27 @@
+from tago import Tago
+from tago.account.analysis import Analysis as Analysis 
+import os
+
+TOKEN = os.environ.get('TAGO_TOKEN_ACCOUNT') or 'TOKEN'
+DEBUG_MESSAGE = "The response to {} \n{}"
+
+def test_analysis_create_delete():
+    assert False
+
+def test_analysis_edit():
+    assert False
+
+def test_analysis_info():
+    assert False
+
+def test_analysis_tokenGenerate():
+    assert False
+
+def test_analysis_uploadFile():
+    assert False
+
+def test_analysis_run():
+    assert False
+
+def test_analysis_listening():
+    assert False
