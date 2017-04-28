ee86cf3e783cd147995129cfd5d1d59869948efa
Kushagra Mishra <kmishra@ncsu.edu>
2017-04-27 21:09:47 -0400

    moving tests

--- tests/services/test_sms.py
+++ tests/services/test_sms.py
@@ -1,10 +1,11 @@
 from tago import Tago
 import os
+from tago.services.sms import SMS as sms
 
 TOKEN = os.environ.get('TAGO_TOKEN_DEVICE') or 'TOKEN'
 
-def test_insert():
-    result = Tago(TOKEN).services.send({'to': '+19199960103'}, 'message': 'test tago services')
+def test_sms():
+    result = sms(TOKEN).send('+19199960103', 'test tago services')
     print result
     if result['status']:
         assert True
