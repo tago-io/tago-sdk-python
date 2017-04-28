4857f9fedbc0fedd5d7062e09c8d65ede0fdb811
Kushagra Mishra <kmishra@ncsu.edu>
2017-04-28 16:55:02 -0400

    adding analysis token for tests and test for sockets

--- tests/services/test_email.py
+++ tests/services/test_email.py
@@ -2,7 +2,7 @@ from tago import Tago
 import os
 from tago.services.email import Email as email
 
-TOKEN = os.environ.get('TAGO_TOKEN_ANALYSIS') or 'TOKEN'
+TOKEN = os.environ.get('TAGO_TOKEN_ANALYSIS') or 'f0ba1f34-2bec-4cba-80ba-088624e37fb2'
 
 def test_email():
     result = email(TOKEN).send('xyz@ncsu.edu', 'tago test', 'test tago services', 'xyz@ncsu.edu', '')
--- tests/services/test_socket.py
+++ tests/services/test_socket.py
@@ -1,11 +1,11 @@
 from tago import Tago
 import os
-from tago.services.console import Console as console
+from tago.services.socket import Socket as socket
 
 TOKEN = os.environ.get('TAGO_TOKEN_ANALYSIS') or 'f0ba1f34-2bec-4cba-80ba-088624e37fb2'
 
-def test_console():
-    result = console(TOKEN).send('test tago services', '')
+def test_socket():
+    result = socket(TOKEN).send('588ff56e3a57780ce6ab7112', '{"variable":"temperature","value":27,"unit":"F"}')
     print result
     if result['status']:
         assert True
