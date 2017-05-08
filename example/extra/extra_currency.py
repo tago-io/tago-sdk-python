b541d401f1c36ecf5335047d7adb897aeb6b6e60
Kushagra Mishra <kmishra@ncsu.edu>
2017-05-08 16:55:30 -0400

    updating print format for extra example script

--- example/extra/extra_currency.py
+++ example/extra/extra_currency.py
@@ -6,9 +6,13 @@ import os
 
 TOKEN = '37dc5a45d8e2596727edfbedc2971ebb'
 
+def print_result(result):
+	for key, value in result.iteritems():
+		print key + " : ", value
+
 def test_convert():
 	result = Tago(TOKEN).extra.currency().convert('USD', 'GBP')
 	print "Convert USD to GBP:"
-	print result
+	print_result(result)
 
 test_convert()
--- example/extra/extra_distance.py
+++ example/extra/extra_distance.py
@@ -6,9 +6,13 @@ import os
 
 TOKEN = 'AIzaSyAJSOIe3c2vxYXa0YDtnf15JFkuRK-3tIk'
 
+def print_result(result):
+	for key, value in result.iteritems():
+		print key + " : ", value
+
 def test_measure():
 	result = Tago(TOKEN).extra.distance().measure('35.7706256,-78.6952835', '35.7728213,-78.6865257', '', '')
 	print "Distance Test:"
-	print result
+	print_result(result)
 
 test_measure()
