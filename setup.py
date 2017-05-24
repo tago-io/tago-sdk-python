0ce06f1993a2bb85ee4c12c34b366ded8768ddd9
Kushagra Mishra <kushagra.om@gmail.com>
2017-05-24 19:21:55 -0400

    updating the version to the new release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tago',
-      version='1.0.9',
+      version='2.0.0',
       description='Official Python lib for Tago',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
